20f60fa826d6d1763b2254fd2855981c73f7eec7
edigabrielcontato@hotmail.com <104377342+EdiGabriel22@users.noreply.github.com>
2024-03-02 13:13:25 -0800

    feat: setup shadcn-ui

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1 class="text-green-600 font-bold text-2xl font-[Inter] text-center">Nuxt Instants</h1>
+    <Button>Instants</Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,21 @@ export default defineNuxtConfig({
 
   modules: [
     "@nuxtjs/tailwindcss",
-    "@nuxtjs/google-fonts"
+    "@nuxtjs/google-fonts",
+    "shadcn-nuxt"
   ],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
 
   googleFonts: {
     base64: true,
